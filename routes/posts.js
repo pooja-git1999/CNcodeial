111ded4a04db79f2561f4e3f848a2b402d43a67e
pooja kumawat <pkumawat009876@gmail.com>
2022-11-29 15:18:43 +0530

    going to add ejs now

---
+++ routes/posts.js
@@ -0,0 +1,11 @@
+// const express = require('express');
+// const router = express.Router();
+// const postController = require('../controllers/users_controller');
+
+// console.log('router loaded');
+
+
+// router.get('/posts', postController.posts);
+
+
+// module.exports = router;
