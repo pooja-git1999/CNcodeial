94141aa3d07a2e58e90964a9b76de822d60b43ef
pooja kumawat <pkumawat009876@gmail.com>
2022-11-30 17:06:23 +0530

    partials is added and working now we add layouts

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,6 @@
 module.exports.home = function(req, res){
    // return res.end('<h1>Express is up for Codeial!</h1>')
-   return res.render('home',{
+   return res.render('home',{//user_profile is name of ejs file
     title: "Home"
    });
 }
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,6 @@
-module.exports.profile = function (req,res) {
-    res.end('<h1>user profile</h1>');
+module.exports.profile = function (req, res) {
+    // res.end('<h1>user profile</h1>');
+    return res.render('user_profile', {//user_profile is name of ejs file
+        title: "user profile"
+    });
 }
