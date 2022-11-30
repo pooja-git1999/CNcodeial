98b5eb28229abffa85925fe36c6d1d265ae308d6
pooja kumawat <pkumawat009876@gmail.com>
2022-11-30 13:04:00 +0530

    ejs added

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const port = 8000;
 //use express router
 app.use('/', require('./routes'));
 
+//setting view engine ejs
+app.set('view engine','ejs');
+app.set('views','./views');
+
+
 app.listen(port, function (err) {
     if (err) {
         console.log(`Error in running the server: ${err}`);
