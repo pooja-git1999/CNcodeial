457e407c6952a6f2c7f54baf6a345c81f5ea6111
pooja kumawat <pkumawat009876@gmail.com>
2022-12-16 16:24:50 +0530

    passport complete

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,9 @@
-module.exports.home = function(req, res){
+module.exports.home = function (req, res) {
    // return res.end('<h1>Express is up for Codeial!</h1>')
    console.log(req.cookies);
-   res.cookie('user_id:1',25);//change the value of cookie
-   return res.render('home',{//user_profile is name of ejs file
-    title: "Home"
+   res.cookie('user_id:1', 25);//change the value of cookie
+   return res.render('home', {
+      title: "Home"
    });
 }
+// module.exports.actionName = function(req, res){}
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,42 +1,33 @@
 const User = require('../models/user');
 
 module.exports.profile = function (req, res) {
-    // res.end('<h1>user profile</h1>');
-    // return res.render('user_profile', {//user_profile is name of ejs file
-    //     title: "user profile"
-    // });
-    if (req.cookies.user_id) {
-        User.findById(req.cookies.user_id,function (err,user) {
-          if(user) {
-            return res.render('user_profile', {//user_profile is name of ejs file
-                   title: "user Profile",
-                   user:user
-                 });
-          }
-          return res.redirect('/users/sign-in');
-        });
-    }else{
-        return res.redirect('/users/sign-in');
-    }
+    return res.render('user_profile', {//user_profile is name of ejs file
+        title: "User Profile"
+    });
 }
 
-//render sign up
+//render the sign up page
 module.exports.signUp = function (req, res) {
     return res.render('user_sign_up', {
         title: "Codeial | Sign Up"
     });
 }
-//render sign in
+
+
+//render the sign in page
 module.exports.signIn = function (req, res) {
     return res.render('user_sign_in', {
         title: "Codeial | Sign In"
     });
 }
+
+
 //get the sign up data
 module.exports.create = function (req, res) {
     if (req.body.password != req.body.confirm_password) {
         return res.redirect('back');
     }
+
     User.findOne({ email: req.body.email }, function (err, user) {
         if (err) { console.log('error in finding user in signing up'); return }
         if (!user) {
@@ -53,27 +44,10 @@ module.exports.create = function (req, res) {
 
 //sign in and create a session for the user
 module.exports.createSession = function (req, res) {
-    //steps to authenticate
-    //find the user
-    User.findOne({ email: req.body.email }, function (err, user) {
-        if (err) { console.log('error in finding user in signing in'); return }
-        //handle user found
-        if (user) {
+    return res.redirect('/');
+}
 
-            //handle password which don't match
-            if (user.password != req.body.password) {
-                return res.redirect('back');
-            }
-            //handle session creation
-            res.cookie('user_id', user.id);
-            return res.redirect('/users/profile');
-        } else {
-
-            //handle user not found
-            return res.redirect('back')
-
-        }
-
-    })
-
+module.exports.destroySession = function (req, res) {
+    req.logout();
+    return res.redirect('/');
 }
