f2a0b1a972efc37887f7f6d9c11d2f851fb56001
pooja kumawat <pkumawat009876@gmail.com>
2023-01-21 17:22:31 +0530

    user profile link and updating a user's profile

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,4 +1,6 @@
 const Post = require('../models/post');
+const User = require('../models/user');
+
 module.exports.home =  function (req, res) {
    //  console.log(req.cookies);
    //  res.cookie('user_id', 25);//change the value of cookie
@@ -20,11 +22,15 @@ module.exports.home =  function (req, res) {
         }
     })
     .exec(function (err, posts) {  
-        return res.render('home', {
-            title: "Codeial | Home",
-            posts: posts
+        User.find({},function (err,users) {
+            return res.render('home', {
+                title: "Codeial | Home",
+                posts: posts,
+                all_users: users
+            });  
         });
-     })
+       
+     });
 
 }
 
