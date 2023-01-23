8ec835139a76d14e92b14d1e09e4fd0d86bad10d
pooja kumawat <pkumawat009876@gmail.com>
2023-01-23 21:26:02 +0530

    async await added

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -70,16 +70,15 @@ module.exports.create = function (req, res) {
 
 // sign in and create a session for the user
 module.exports.createSession = function (req, res) {
+    req.flash('success', 'Logged in successfully')
     return res.redirect('/');
 }
 
-module.exports.destroySession = function (req, res, next) {
-    req.logout(function (err) {
-        if (err) {
-            return next(err);
-        }
-        return res.redirect('/');
-    })
+module.exports.destroySession = function (req, res) {
+    req.logout();
+    req.flash('success', ' you have Logged out!');
+
+    return res.redirect('/');
 
 
 }
--- index.js
+++ index.js
@@ -11,14 +11,16 @@ const passportLocal = require('./config/passport-local-strategy');
 const MongoStore = require('connect-mongo');
 //using sass
 const sassMiddleware = require('node-sass-middleware');
+const flash = require('connect-flash');
+const customMware = require('./config/middleware')
 
 app.use(sassMiddleware({
     /* Options */
     src: './assets/sass',
-   dest: './assets/css',
-   debug: true,
-   outputStyle: 'extended',
-   prefix: '/css'
+    dest: './assets/css',
+    debug: true,
+    outputStyle: 'extended',
+    prefix: '/css'
 }));
 
 app.use(express.urlencoded());
@@ -70,7 +72,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(passport.setAuthenticatedUser);
-
+app.use(flash());
+app.use(customMware.setFlash);
 // use express router
 app.use('/', require('./routes'));
 
