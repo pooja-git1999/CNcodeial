cf1d3813f140c5f9ce09ec4e41d33a0b859503f4
pooja kumawat <pkumawat009876@gmail.com>
2023-01-30 17:04:04 +0530

    async await complete

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -7,12 +7,15 @@ module.exports.create = async function (req, res) {
             content: req.body.content,
             user: req.user._id
         });
+        req.flash('success', 'Post published');
         return res.redirect('back');
 
 
     } catch (err) {
-        console.log('error', err);
-        return;
+        //console.log('error', err);
+        req.flash('error', err);
+        return res.redirect('back');
+
     }
 
 }
@@ -24,13 +27,17 @@ module.exports.destory = async function (req, res) {
         if (post.user == req.user.id) {
             post.remove();
             await Comment.deleteMany({ post: req.params.id });
+            req.flash('success', 'Post and associated comments deleted');
             return res.redirect('back');
         } else {
+            req.flash('error', 'you cannot delete this post');
             return res.redirect('back');
         }
     } catch (err) {
-        console.log('error', err);
-        return;
+        // console.log('error', err);
+        req.flash('error', err);
+
+        return res.redirect('back');
     }
 
 }
