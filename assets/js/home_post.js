4d70ca94fed5b8db7fa538f80be7b7aa845e512b
pooja kumawat <pkumawat009876@gmail.com>
2023-02-18 13:24:03 +0530

    deleting the post done

--- assets/js/home_post.js
+++ assets/js/home_post.js
@@ -13,6 +13,7 @@
                     //console.log(data);
                     let newPost = newPostDom(data.data.post);
                     $('#posts-list-container>ul').prepend(newPost);
+                    deletePost($(' .delete-post-button',newPost))
                 },
                 error: function (error) {
                     console.log(error.responseText);
@@ -23,33 +24,50 @@
     //method to create a post in dom
     let newPostDom = function (post) {
         return $(`<li id="post-${post._id}">
-        <p>
+     <p>
           <small>
-           <a class="delete-post-button" href="/posts/destroy/${ post.id }">X</a>
+           <a class="delete-post-button" href="/posts/destroy/${post._id}">X</a>
           </small>
-                  ${ post.content }
-                  
-                    <br>
-                    <small>
-                        ${ post.user.name }
-                    </small>
-        </p>
+
+          ${post.content}
+         <br>
+         <small>
+         ${post.user.name}
+        </small>
+    </p>
         <div class="post-comments">
-            <
+
                 <form action="/comments/create" method="post">
-                    <input type="text" name="content" id="" placeholder="your comment">
-                    <input type="hidden" name="post" value="${ post._id }" >
+                    <input type="text" name="content" id="" placeholder="your comment" required>
+                    <input type="hidden" name="post" value="${post._id}" >
                     <input type="submit" value="Add Comment">
                 </form>
                 
     
-                    <div class="post-comments-list">
-                        <ul id="post-comments-${post._id}">
+  <div class="post-comments-list">
+  <ul id="post-comments-${post._id}">
                             
-                        </ul>
-                    </div>
+                 </ul>
+            </div>
         </div>
     </li>`)
     }
+    //method to delete a post from Dom
+    let deletePost = function (deleteLink) {
+        $(deleteLink).click(function (e) {
+            e.preventDefault();
+            $.ajax({
+                type: 'get',
+                url: $(deleteLink).prop('href'),
+                success: function (data) {
+                    $(`#post-${data.data.post_id}`).remove();
+                },error: function (error) {
+                    console.log(error.responseText);
+                }
+            });
+        })
+    }
+
+
     createPost();
 }
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -36,15 +36,25 @@ module.exports.destory = async function (req, res) {
         if (post.user == req.user.id) {
             post.remove();
             await Comment.deleteMany({ post: req.params.id });
-           // req.flash('success', 'Post and associated comments deleted');
+           
+            if(req.xhr){
+                return res.status(200).json({
+                    data:{
+                        post_id: req.params.id
+                    },
+                    message:"post deleted !"
+                })
+            }
+           
+            req.flash('success', 'Post and associated comments deleted');
             return res.redirect('back');
         } else {
-            //req.flash('error', 'you cannot delete this post');
+            req.flash('error', 'you cannot delete this post');
             return res.redirect('back');
         }
     } catch (err) {
-        console.log('error', err);
-        //req.flash('error', err);
+       // console.log('error', err);
+        req.flash('error', err);
 
         return res.redirect('back');
     }
