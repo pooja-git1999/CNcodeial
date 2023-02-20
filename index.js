0796c2047d7017e883fd22671216034e594500ad
pooja kumawat <pkumawat009876@gmail.com>
2023-02-20 16:46:19 +0530

    edge case done

--- index.js
+++ index.js
@@ -28,14 +28,13 @@ app.use(express.urlencoded());
 app.use(cookieParser());
 
 app.use(express.static('./assets'));
-
+//make the uploads path available to the browser
+app.use('/uploads',express.static(__dirname + '/uploads'));
 app.use(expressLayouts);
 
 // extract style and scripts from sub pages into the layout
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
-
-
 
 
 // set up the view engine
