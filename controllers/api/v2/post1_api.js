de5c76b792fcd9604f9f15cc2f60eed0f321d473
pooja kumawat <pkumawat009876@gmail.com>
2023-02-21 16:05:03 +0530

    playing with api

--- controllers/api/v2/post1_api.js
+++
@@ -1,6 +0,0 @@
-module.exports.index_1 = function (req,res) {
-    return res.json(200, {
-        message: " New list of posts",
-        posts: []
-    })
-}
--- routes/api/v2/index.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-router.use('/posts_1',require('./posts_1'));
-
-module.exports = router;
